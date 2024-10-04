025c4eba5fc24eca089e8ee50896fd8bf06f0667
prattushCrewnox <prattushcrewnox@gmail.com>
2024-10-04 20:29:10 +0530

    final hero section

--- src/assets/js/about.js
+++ src/assets/js/about.js
@@ -1,87 +1,95 @@
+window.addEventListener('scroll', function() {
+    const videoElement = document.querySelector('.bg-video');
+    if (window.scrollY > 1000) {
+        console.log("worked     ")
+      videoElement.style.display = 'none'; // Or remove a specific style as needed
+    } else {
+      videoElement.style.display = 'block'; // Restore the style when scrolled back up
+    }
+  });
 
 $(function () {
+  "use strict";
 
-    "use strict";
+  /* ===============================  Carousel slider  =============================== */
 
+  var galleryText = new Swiper(".works-ma .gallery-text .swiper-container", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    direction: "vertical",
+    loop: true,
+    loopedSlides: 4,
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+    speed: 1500,
+  });
 
-    /* ===============================  Carousel slider  =============================== */
+  var galleryImg = new Swiper(".works-ma .gallery-img .swiper-container", {
+    spaceBetween: 40,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 4,
+    speed: 1500,
+    navigation: {
+      nextEl: ".works-ma .swiper-controls .swiper-button-next",
+      prevEl: ".works-ma .swiper-controls .swiper-button-prev",
+    },
+    pagination: {
+      el: ".works-ma .swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return (
+          '<span class="' +
+          className +
+          '">' +
+          '<svg class="fp-arc-loader" width="16" height="16" viewBox="0 0 16 16">' +
+          '<circle class="path" cx="8" cy="8" r="5.5" fill="none" transform="rotate(-90 8 8)" stroke="#FFF"' +
+          'stroke-opacity="1" stroke-width="1px"></circle>' +
+          '<circle cx="8" cy="8" r="3" fill="#FFF"></circle>' +
+          "</svg></span>"
+        );
+      },
+    },
+    keyboard: {
+      enabled: true,
+    },
 
-    var galleryText = new Swiper('.works-ma .gallery-text .swiper-container', {
-        spaceBetween: 30,
+    breakpoints: {
+      0: {
         slidesPerView: 1,
-        direction: 'vertical',
-        loop: true,
-        loopedSlides: 4,
-        touchRatio: 0.2,
-        slideToClickedSlide: true,
-        speed: 1500,
-    });
-
-    var galleryImg = new Swiper('.works-ma .gallery-img .swiper-container', {
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
         spaceBetween: 40,
-        centeredSlides: true,
-        loop: true,
-        loopedSlides: 4,
-        speed: 1500,
-        navigation: {
-            nextEl: '.works-ma .swiper-controls .swiper-button-next',
-            prevEl: '.works-ma .swiper-controls .swiper-button-prev',
-        },
-        pagination: {
-            el: '.works-ma .swiper-pagination',
-            clickable: true,
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '">' + '<svg class="fp-arc-loader" width="16" height="16" viewBox="0 0 16 16">' +
-                    '<circle class="path" cx="8" cy="8" r="5.5" fill="none" transform="rotate(-90 8 8)" stroke="#FFF"' +
-                    'stroke-opacity="1" stroke-width="1px"></circle>' +
-                    '<circle cx="8" cy="8" r="3" fill="#FFF"></circle>' +
-                    '</svg></span>';
-            },
+      },
+      1024: {
+        slidesPerView: 2,
+      },
+    },
+  });
 
-        },
-        keyboard: {
-            enabled: true,
-        },
+  galleryImg.on("slideChangeTransitionStart", function () {
+    galleryText.slideTo(galleryImg.activeIndex);
+  });
+  galleryText.on("transitionStart", function () {
+    galleryImg.slideTo(galleryText.activeIndex);
+  });
 
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-                spaceBetween: 30,
-            },
-            640: {
-                slidesPerView: 1,
-                spaceBetween: 30,
-            },
-            768: {
-                slidesPerView: 1,
-                spaceBetween: 40,
-            },
-            1024: {
-                slidesPerView: 2,
-            },
-        }
+  if (window.innerWidth > 991) {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".about-ma .bg-img",
+        start: "top",
+        endTrigger: ".about-ma",
+        end: "bottom bottom",
+        pin: true,
+        pinSpacing: false,
+      },
     });
-
-    galleryImg.on("slideChangeTransitionStart", function () {
-        galleryText.slideTo(galleryImg.activeIndex);
-    });
-    galleryText.on("transitionStart", function () {
-        galleryImg.slideTo(galleryText.activeIndex);
-    });
-
-
-    if (window.innerWidth > 991) {
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".about-ma .bg-img",
-                start: "top",
-                endTrigger: ".about-ma",
-                end: "bottom bottom",
-                pin: true, 
-                pinSpacing: false
-            }
-        });
-    } 
-
-
+  }
 });
